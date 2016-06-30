2b609c5b466bb3e6cab983e1a128c94cdfc93a29
N Vempati <contact.vempati@gmail.com>
2016-06-30 12:15:56 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## cache the matrix orinial or inverse when solved
 
 makeCacheMatrix <- function(x = matrix()) {
+    matrixinv <- NULL ##value assigned at current (parent) level
+    
+    set <- function(y) { ## closure function
+        x <<- y
+        matrixinv <<- NULL ##assign NULL to the final result variable by modifying the parent value assignent
+    }
+    get <- function() x
 
+    setInverse <- function(inverse) matrixinv <<- inverse
+    
+    getInverse <- function() matrixinv
+    list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## Get matrix from parent and solve/inverse the original matrix from parent
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    matrixinv <- x$getInverse()
+    if (!is.null(matrixinv)) {
+        message("getting cached data")
+        return(matrixinv)
+    }
+    originalmatrix <- x$get()
+    matrixinv <- solve(originalmatrix, ...)
+    x$setInverse(matrixinv)
+    matrixinv
 }
